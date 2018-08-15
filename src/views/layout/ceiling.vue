aa76c00c1b9d743a1a732ef4b531b14e2c97a1e6
ZhiyiFu <513432821@qq.com>
2018-08-15 10:21:24 +0800

    Layout

--- src/views/layout/ceiling.vue
+++ src/views/layout/ceiling.vue
@@ -2,7 +2,6 @@
   <div>
     <div class="layout-ceiling">
       <v-toolbar height="40px">
-        <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title class="height--text">地理大数据时空解析原型系统</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
@@ -13,11 +12,17 @@
         </v-toolbar-items>
       </v-toolbar>
     </div>
-    <v-container grid-list-xl text-xs-center>
-      <user-dialog :dialogVisible="dialogVisible" ></user-dialog>
-    </v-container>
+
+    <div>
+      <!--container会使面包屑等不显示-->
+      <!--<v-container grid-list-xl text-xs-center>-->
+        <user-dialog :dialogVisible="dialogVisible"></user-dialog>
+      <!--</v-container>-->
+    </div>
+
   </div>
 </template>
+
 <style scoped >
   .layout-ceiling {
     color: #4a4e55;
@@ -27,12 +32,44 @@
     overflow: hidden;
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
   }
+
+
+
+  /*.layout-ceiling-main a {*/
+    /*border-left: 1px solid #cccccc;*/
+    /*color: #9ba7b5;*/
+    /*padding: 0 10px;*/
+    /*margin: 10px 0;*/
+    /*text-align: center;*/
+    /*cursor: pointer;*/
+    /*font-size: 14px;*/
+    /*line-height: 14px;*/
+    /*position: relative;*/
+  /*}*/
+
+  /*.layout-ceiling-main a:hover {*/
+    /*color: #fff;*/
+  /*}*/
+  /*.layout-ceiling .avatar {*/
+    /*float: right;*/
+    /*margin-right: 1em*/
+  /*}*/
+  /*.layout-ceiling .avatar img {*/
+    /*width: 40px;*/
+    /*height: 40px;*/
+    /*border-radius: 20px;*/
+    /*margin-left: 10px;*/
+    /*float: right;*/
+  /*}*/
+
+
 </style>
 <script>
+
   import userDialog from "../../components/dialog/userDialog";
   export default {
     name: 'ceiling',
-    components:{
+    components: {
       "user-dialog": userDialog
     },
     data: () => ({
@@ -42,13 +79,13 @@
         '帮助',
         '退出'
       ],
-      dialogVisible:{
-        v:false,
-        clickModalClose:false
+      dialogVisible: {
+        v: false,
+        clickModalClose: false
       }
     }),
-    methods:{
-      userLogin(){
+    methods: {
+      userLogin() {
         this.dialogVisible.v = true
       }
     }
