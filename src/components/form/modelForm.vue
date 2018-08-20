5800d03204b1e3661c92d83155ac79c5033491c4
ZhiyiFu <513432821@qq.com>
2018-08-20 15:10:27 +0800

    fzy 20180820

--- src/components/form/modelForm.vue
+++ src/components/form/modelForm.vue
@@ -5,6 +5,10 @@
       <el-form-item  v-for="(item,i) in form_list" :key="item.name" :label="item.name">
         <el-input v-model='paramsFormData[i]' :placeholder="'请输入'+item.name"></el-input>
       </el-form-item>
+      <!--<el-form-item>-->
+        <!--<el-button type="success" @click="submit">提交</el-button>-->
+        <!--<el-button type="primary" @click="resetForm('ruleForm')">重置</el-button>-->
+      <!--</el-form-item>-->
     </el-form>
   </div>
 </template>
@@ -15,6 +19,12 @@
     data() {
       return {
         paramsFormData: [],
+        // rules:{
+        //   forall:[{required:true,message:'请正确填写参数'}]
+        // },
+        // ruleForm:{
+        //   forall:''
+        // }
       }
     },
     props: {form_list: Array},
@@ -30,6 +40,35 @@
       clearData() {
         this.paramsFormData = [];
       },
+      // resetForm(formName) {
+      //   this.$refs[formName].resetFields();
+      // },
+      // submit() {
+      //   let obj = this;
+      //   let modelparamData = {};
+      //   modelparamData.user = "ubt";
+      //   let params = this.getdata();
+      //   console.log(params)
+      //   modelparamData.params = params;
+      //   modelparamData.customname = params["1"];
+      //   console.log(modelparamData)
+      //   this.$axios.post(
+      //     // url: 'http://192.168.240.25:3000/dldsj/parallel/use/' + this.artifactId,
+      //     'http://192.168.1.5:8080/dldsj/parallel/use/' + this.artifactId,
+      //     modelparamData
+      //   ).then(function (response) {
+      //     // alert(''.concat(response.data.code));
+      //     if(response.code=='200'){
+      //       obj.$message({
+      //         type: 'success',
+      //         message: '模型提交成功!请前往模型项目查看运行进度！'
+      //       });
+      //     }
+      //   }).catch(function (error) {
+      //     obj.$message.error('模型提交失败!');
+      //   });
+      //   this.$emit("dialogVisible.v=false");
+      // },
 
     },
     mounted() {
